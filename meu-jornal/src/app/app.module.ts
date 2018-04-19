85eafa964b0752d41d6a4416b20598e8e6820ca6
Luana <luana_cherri@hotmail.com>
2018-04-19 17:25:23 -0300

    add modulo noticia

--- meu-jornal/src/app/app.module.ts
+++ meu-jornal/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { NoticiaComponent } from './noticia/noticia.component';
-import { AlertaComponent } from './alerta/alerta.component';
-import { PrimeiroExemploComponent } from './primeiro-exemplo/primeiro-exemplo.component';
-import { SegundoExemploComponent } from './segundo-exemplo/segundo-exemplo.component';
-import { TerceiroExemploComponent } from './terceiro-exemplo/terceiro-exemplo.component';
+
+
+import { NoticiaModule } from './noticia/noticia.module';
 
 
 
@@ -18,16 +16,12 @@ import { TerceiroExemploComponent } from './terceiro-exemplo/terceiro-exemplo.co
   declarations: [
     AppComponent,
     TopoComponent,
-    RodapeComponent,
-    NoticiaComponent,
-    AlertaComponent,
-    PrimeiroExemploComponent,
-    SegundoExemploComponent,
-    TerceiroExemploComponent
+    RodapeComponent
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    NoticiaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
